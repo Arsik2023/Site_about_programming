f76cb3b58f03442a65b443b10a4dee1f185f322f
Arsik2023 <137551937+Arsik2023@users.noreply.github.com>
2024-06-27 10:45:04 +0500

    Add files via upload

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_1.Models;
+
+namespace WebApplication_1.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
